29a36b9a99c1a4d70280f9da8d73ce71724a3584
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2025-11-29 00:09:04 -0300

    feat: repository implementation

---
+++ src/main/java/com/loomi/order_processor/infra/persistence/product/ProductRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.loomi.order_processor.infra.persistence.product;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import com.loomi.order_processor.domain.product.entity.Product;
+import com.loomi.order_processor.domain.product.repository.ProductRepository;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductRepositoryImpl implements ProductRepository {
+
+    private final JpaProductRepository jpaProductRepository;
+
+    @Override
+    public Optional<Product> findById(@NonNull UUID id) {
+       return jpaProductRepository.findById(id);
+    }
+
+    @Override
+    public Product save(@NonNull Product product) {
+        return jpaProductRepository.save(product);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return jpaProductRepository.findAll();
+    }
+
+    @Override
+    public void update(@NonNull Product product) {
+        jpaProductRepository.save(product);
+    }
+    
+}
