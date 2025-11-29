95a134b6ccd9a4119a1ab8924088c2c1457a388c
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2025-11-29 13:01:17 -0300

    feat: order service initial implementation

---
+++ src/main/java/com/loomi/order_processor/app/service/OrderServiceImpl.java
@@ -0,0 +1,73 @@
+package com.loomi.order_processor.app.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.loomi.order_processor.app.utils.JsonUtils;
+import com.loomi.order_processor.domain.order.dto.CreateOrder;
+import com.loomi.order_processor.domain.order.dto.OrderItem;
+import com.loomi.order_processor.domain.order.entity.Order;
+import com.loomi.order_processor.domain.order.exception.OrderNotFoundException;
+import com.loomi.order_processor.domain.order.repository.OrderRepository;
+import com.loomi.order_processor.domain.order.service.OrderService;
+import com.loomi.order_processor.domain.product.dto.ValidationResult;
+import com.loomi.order_processor.domain.product.dto.ValidatorMap;
+import com.loomi.order_processor.domain.product.exception.ProductValidationException;
+import com.loomi.order_processor.domain.product.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final ValidatorMap validatorMap;
+
+    @Override
+    public Order consultOrder(UUID orderId) {
+        return orderRepository.findById(orderId)
+            .orElseThrow(() -> new OrderNotFoundException(orderId));
+    }
+
+    @Override
+    public void createOrder(CreateOrder createOrder) {
+        var validationErrors = new ArrayList<ValidationResult>();
+
+        for(var item : createOrder.items()) {
+            var results = validateOrderItem(item);
+            if(!results.isEmpty()) {
+                validationErrors.addAll(results);
+            }
+        }
+
+        if(!validationErrors.isEmpty()) {
+            throw new ProductValidationException(JsonUtils.toJson(validationErrors));
+        }
+
+        throw new UnsupportedOperationException("Unfinished method 'createOrder'");
+    }
+
+    private List<ValidationResult> validateOrderItem(OrderItem item) {
+        var product = productRepository.findById(item.productId()).orElseThrow();
+        var productValidators = validatorMap.getValidatorsFor(product);
+
+        List<ValidationResult> results = new ArrayList<>();
+
+        for(var validator : productValidators) {
+            var result = validator.validate(product);
+            if(!result.isValid()) {
+                results.add(result);
+            }
+        }
+
+        return results;
+    } 
+
+}
