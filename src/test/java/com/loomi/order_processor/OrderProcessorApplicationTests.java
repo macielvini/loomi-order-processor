60dd8043e5cecd52d7dbe13811b10f848d70f878
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2025-11-29 20:02:56 -0300

    test(fix): remove kafka admin error

--- src/test/java/com/loomi/order_processor/OrderProcessorApplicationTests.java
+++ src/test/java/com/loomi/order_processor/OrderProcessorApplicationTests.java
@@ -3,10 +3,15 @@ package com.loomi.order_processor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class OrderProcessorApplicationTests {
+
+	@MockitoBean
+	private KafkaAdmin kafkaAdmin;
 
 	@Test
 	void contextLoads() {
