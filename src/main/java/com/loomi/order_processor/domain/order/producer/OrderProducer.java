66539c793a148e77102f2cc8b9f2aef28980ddb2
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2025-11-30 17:00:02 -0300

    feat: order producer impl for processed and failed events

--- src/main/java/com/loomi/order_processor/domain/order/producer/OrderProducer.java
+++ src/main/java/com/loomi/order_processor/domain/order/producer/OrderProducer.java
@@ -1,9 +1,14 @@
 package com.loomi.order_processor.domain.order.producer;
 
 import com.loomi.order_processor.domain.order.entity.OrderCreatedEvent;
+import com.loomi.order_processor.domain.order.entity.OrderFailedEvent;
+import com.loomi.order_processor.domain.order.entity.OrderProcessedEvent;
 
 public interface OrderProducer {
 
     void sendOrderCreatedEvent(OrderCreatedEvent event);
     
+    void sendOrderProcessedEvent(OrderProcessedEvent event);
+    
+    void sendOrderFailedEvent(OrderFailedEvent event);
 }
