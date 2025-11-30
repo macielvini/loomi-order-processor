16f876c78ba90f04372d90c6312b2600d33959fd
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2025-11-30 17:33:12 -0300

    feat: is available validator

---
+++ src/main/java/com/loomi/order_processor/domain/order/service/IsAvailableValidator.java
@@ -0,0 +1,41 @@
+package com.loomi.order_processor.domain.order.service;
+
+import org.springframework.stereotype.Component;
+
+import com.loomi.order_processor.domain.order.dto.OrderItem;
+import com.loomi.order_processor.domain.order.dto.OrderItemValidationError;
+import com.loomi.order_processor.domain.order.dto.OrderItemValidationResult;
+import com.loomi.order_processor.domain.product.dto.ProductType;
+import com.loomi.order_processor.domain.product.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+@OrderItemValidatorFor(value = ProductType.PHYSICAL)
+public class IsAvailableValidator implements OrderItemValidator {
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public OrderItemValidationResult validate(OrderItem item) {
+        var optProduct = productRepository.findById(item.productId());
+        if (optProduct.isEmpty()) {
+            return OrderItemValidationResult.error(OrderItemValidationError.INTERNAL_ERROR);
+        }
+
+        var product = optProduct.get();
+
+        if (!product.isActive() || product.stockQuantity() < item.quantity()) {
+            return OrderItemValidationResult.error(OrderItemValidationError.OUT_OF_STOCK);
+        }
+
+        return OrderItemValidationResult.ok();
+    }
+
+    @Override
+    public boolean supports(ProductType type) {
+        return type.equals(ProductType.PHYSICAL);
+    }
+    
+}
