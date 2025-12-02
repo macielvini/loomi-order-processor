c61ee945be7b24c102f3b7713d6276100342abd3
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2025-12-02 09:53:37 -0300

    feat(kafka): update OrderCreatedConsumer to include acknowledgment handling

--- src/main/java/com/loomi/order_processor/domain/order/consumer/OrderCreatedConsumer.java
+++ src/main/java/com/loomi/order_processor/domain/order/consumer/OrderCreatedConsumer.java
@@ -1,7 +1,8 @@
 package com.loomi.order_processor.domain.order.consumer;
 
 import com.loomi.order_processor.domain.order.entity.OrderCreatedEvent;
+import org.springframework.kafka.support.Acknowledgment;
 
 public interface OrderCreatedConsumer {
-    void handler(OrderCreatedEvent event);
+    void handler(OrderCreatedEvent event, Acknowledgment ack);
 }
